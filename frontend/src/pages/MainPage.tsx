04613eb4f1abb3c21f7a96c861a3bc98b243d686
beurmuz <fallinta2@gmail.com>
2023-03-07 16:57:11 +0900

    #3 [dev] 디데이 추가 및 순서 변경

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "../components/Header";
 import styled from "styled-components";
 import backgroundImageUrl from "../assets/images/background.jpg";
+import backImageUrl from "../assets/images/backImage.jpg";
+import faceImageUrl from "../assets/images/faceImage.jpg";
 import Footer from "../components/Footer";
 
 const MainPage = () => {
@@ -10,32 +12,41 @@ const MainPage = () => {
       <MainBox_01>
         <Header />
         <MainContent_01>
-          <PageName>SIK-K</PageName>
+          <HomePageName>SIK-K</HomePageName>
           <ArtistID>@younghotyellow94</ArtistID>
         </MainContent_01>
         <Footer />
       </MainBox_01>
       <MainBox_02>
         <MainContent_02>
-          <PageName>Who is he?</PageName>
+          <PartName>Who is he?</PartName>
           <MainArticles>
-            <IntroArticle>
+            <Article>
+              <FaceImage />
+              <Discription_01>
+                In 2015, He debuted with the song “My Man”. In 2015, He debuted
+                with the song “My Man”.
+              </Discription_01>
+            </Article>
+            <Article>
               In 2015, He debuted with the song “My Man”. In 2015, He debuted
               with the song “My Man”.
-            </IntroArticle>
-            <IntroArticle>
+            </Article>
+            <Article>
               In 2015, He debuted with the song “My Man”. In 2015, He debuted
               with the song “My Man”.
-            </IntroArticle>
-            <IntroArticle>
-              In 2015, He debuted with the song “My Man”. In 2015, He debuted
-              with the song “My Man”.
-            </IntroArticle>
+            </Article>
           </MainArticles>
         </MainContent_02>
       </MainBox_02>
-      {/* <MainBox_03></MainBox_03>
-      <MainBox_04></MainBox_04> */}
+      <MainBox_03>
+        <MainContent_03>
+          <Dday>D+2769</Dday>
+        </MainContent_03>
+      </MainBox_03>
+      <MainBox_04>
+        <GoTopButton>↑ Back to top ↑</GoTopButton>
+      </MainBox_04>
     </MainWrapper>
   );
 };
@@ -67,12 +78,17 @@ const MainContent_01 = styled.div`
   height: 60vh;
 `;
 
-const PageName = styled.h1`
+const HomePageName = styled.h1`
   font-size: 10rem;
   font-weight: 700;
   font-style: italic;
 `;
 
+const PartName = styled.h2`
+  font-size: 8rem;
+  font-weight: 700;
+  font-style: italic;
+`;
 const ArtistID = styled.h3`
   font-size: 2.5rem;
   font-weight: 400;
@@ -102,32 +118,63 @@ const MainArticles = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  border-top: 1px solid gray;
+  // border-top: 1px solid gray;
 `;
 
-const IntroArticle = styled.article`
+const Article = styled.article`
   width: 30%;
   padding: 30px 0px;
 `;
 
-// const MainBox_03 = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   width: 100%;
-//   height: 100vh;
-//   background-color: black;
-//   color: white;
-//   padding: 80px 70px;
-//   border-top: solid 1px white;
-// `;
+const FaceImage = styled.img.attrs({
+  src: `${faceImageUrl}`,
+})`
+  width: 100%;
+`;
 
-// const MainBox_04 = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   width: 100%;
-//   height: 100vh;
-//   background-color: black;
-//   color: white;
-//   padding: 80px 70px;
-//   border-top: solid 1px white;
-// `;
+const Discription_01 = styled.p`
+  width: 100%;
+`;
+
+const MainBox_03 = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  background-color: black;
+  color: white;
+  padding: 80px 70px;
+  border-top: solid 1px white;
+`;
+
+const MainBox_04 = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  background-color: black;
+  color: white;
+  padding: 0px;
+  border-top: solid 1px white;
+`;
+
+const MainContent_03 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const GoTopButton = styled.button`
+  width: 200px;
+  font-size: 1rem;
+  color: gray;
+  // padding:
+`;
+
+const Dday = styled.p`
+  font-weight: 700;
+  font-size: 24rem;
+  color: gray;
+`;

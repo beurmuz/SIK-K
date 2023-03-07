c8afae2dd4ed2089e4d4deb48d2f54e7034badca
beurmuz <fallinta2@gmail.com>
2023-03-07 12:12:21 +0900

    #3 [dev] 메인페이지 2 개발

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -7,12 +7,35 @@ import Footer from "../components/Footer";
 const MainPage = () => {
   return (
     <MainWrapper>
-      <Header />
-      <MainContentWrapper>
-        <PageName>SIK-K</PageName>
-        <ArtistID>@younghotyellow94</ArtistID>
-      </MainContentWrapper>
-      <Footer />
+      <MainBox_01>
+        <Header />
+        <MainContent_01>
+          <PageName>SIK-K</PageName>
+          <ArtistID>@younghotyellow94</ArtistID>
+        </MainContent_01>
+        <Footer />
+      </MainBox_01>
+      <MainBox_02>
+        <MainContent_02>
+          <PageName>Who is he?</PageName>
+          <MainArticles>
+            <IntroArticle>
+              In 2015, He debuted with the song “My Man”. In 2015, He debuted
+              with the song “My Man”.
+            </IntroArticle>
+            <IntroArticle>
+              In 2015, He debuted with the song “My Man”. In 2015, He debuted
+              with the song “My Man”.
+            </IntroArticle>
+            <IntroArticle>
+              In 2015, He debuted with the song “My Man”. In 2015, He debuted
+              with the song “My Man”.
+            </IntroArticle>
+          </MainArticles>
+        </MainContent_02>
+      </MainBox_02>
+      {/* <MainBox_03></MainBox_03>
+      <MainBox_04></MainBox_04> */}
     </MainWrapper>
   );
 };
@@ -23,14 +46,20 @@ const MainWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 100vw;
+`;
+
+const MainBox_01 = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
   height: 100vh;
   background: url(${backgroundImageUrl}) no-repeat center;
   background-size: cover;
   color: white;
-  padding: 50px;
+  padding: 70px;
 `;
 
-const MainContentWrapper = styled.div`
+const MainContent_01 = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -49,3 +78,56 @@ const ArtistID = styled.h3`
   font-weight: 400;
   color: gray;
 `;
+
+const MainBox_02 = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  background-color: black;
+  color: white;
+  padding: 80px 70px;
+`;
+
+const MainContent_02 = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  // height: 60vh;
+`;
+
+const MainArticles = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  border-top: 1px solid gray;
+`;
+
+const IntroArticle = styled.article`
+  width: 30%;
+  padding: 30px 0px;
+`;
+
+// const MainBox_03 = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   width: 100%;
+//   height: 100vh;
+//   background-color: black;
+//   color: white;
+//   padding: 80px 70px;
+//   border-top: solid 1px white;
+// `;
+
+// const MainBox_04 = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   width: 100%;
+//   height: 100vh;
+//   background-color: black;
+//   color: white;
+//   padding: 80px 70px;
+//   border-top: solid 1px white;
+// `;

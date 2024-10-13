8ace50cb0efab4ca88c367c778b46817576752e9
beurmuz <fallinta2@gmail.com>
2024-10-14 02:59:02 +0900

    #2 [temp] 임시 코드 작성

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,26 @@ const cx = classNames.bind(styles);
 function App() {
   return (
     <div className={cx("container")}>
-      <div>초기세팅 중입니다.</div>
-      <button>새롭게 시작!</button>
+      <nav className={cx("wrap-nav")}>
+        <h1 className={cx("pagename")}>SIK-K</h1>
+        <ul className={cx("wrap-list")}>
+          <li className={cx("list-item")}>1</li>
+          <li className={cx("list-item")}>2</li>
+          <li className={cx("list-item")}>3</li>
+        </ul>
+      </nav>
+      <div className={cx("iframe-container")}>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/2SslHKdy2vM?autoplay=1&mute=1&si=HaLZI2zhWmTz3bZR&amp;controls=0"
+          title="SIK-K TRAP"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
     </div>
   );
 }
